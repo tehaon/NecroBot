3fad3fba83ab1a8d837cd9ef558f8d383debfe02
kenny.brown <kenny.brown@VUHL-B9SQK12>
2016-07-29 14:40:25 -0500

    ui

---
+++ PoGo.Necrobot.UI/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+
+namespace PoGo.Necrobot.UI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            try
+            {
+                System.Net.IPHostEntry e =
+                     System.Net.Dns.GetHostEntry("www.google.com");
+            }
+            catch
+            {
+                MainMap.Manager.Mode = AccessMode.CacheOnly;
+                MessageBox.Show("No internet connection avaible, going to CacheOnly mode.",
+                      "GMap.NET - Demo.WindowsForms", MessageBoxButtons.OK,
+                      MessageBoxIcon.Warning);
+            }
+
+            //http://www.codeproject.com/Articles/32643/GMap-NET-Great-Maps-for-Windows-Forms-and-Presenta
+            // config map
+            MainMap.MapProvider = GMapProviders.OpenStreetMap;
+            MainMap.Position = new PointLatLng(38.776927, -92.258670);
+            MainMap.MinZoom = 0;
+            MainMap.MaxZoom = 24;
+            MainMap.Zoom = 9;
+
+            //http://greatmaps.codeplex.com/wikipage?title=GMap.NET.WindowsForms&referringTitle=Documentation
+            //http://www.independent-software.com/gmap-net-tutorial-maps-markers-and-polygons/
+            GMapOverlay markersOverlay = new GMapOverlay("markers");
+            GMarkerGoogle marker = new GMarkerGoogle(new PointLatLng(38.776927, -92.258669),
+              GMarkerGoogleType.blue_small);
+            markersOverlay.Markers.Add(marker);
+            MainMap.Overlays.Add(markersOverlay);
+        }
+    }
+}
